635a060ac8f67b96e307a496998eff0098e1b155
keigo1216 <sagankeigo17@gmail.com>
2023-05-09 18:49:06 +0900

    update servo motor program and stepping motor program for moving right motor

--- stepping-motor-sample/stepping-motor-sample.ino
+++ stepping-motor-sample/stepping-motor-sample.ino
@@ -1,32 +1,57 @@
-const int DIR  = 4;
-const int STEP = 5;
+const int DIR_L  = 4;
+const int STEP_L = 5;
+const int DIR_R = 6;
+const int STEP_R = 7;
 
 void setup() {
-  pinMode(DIR, OUTPUT);
-  pinMode(STEP, OUTPUT);
-  digitalWrite(DIR, LOW);
-  digitalWrite(STEP, LOW);
+  // set up left stepping motor
+  pinMode(DIR_L, OUTPUT);
+  pinMode(STEP_L, OUTPUT);
+  digitalWrite(DIR_L, LOW);
+  digitalWrite(STEP_L, LOW);
+
+  // set up right stepping motor
+  pinMode(DIR_R, OUTPUT);
+  pinMode(STEP_R, OUTPUT);
+  digitalWrite(DIR_R, LOW);
+  digitalWrite(STEP_R, LOW);
 }
 
 void loop() {
   
-  digitalWrite(DIR, HIGH);
+  digitalWrite(DIR_L, HIGH);
+  digitalWrite(DIR_R, HIGH);
 
   for (int i=0; i<200; i++) {
-    digitalWrite(STEP, HIGH);
+    // rotate light stepping motor
+    digitalWrite(STEP_L, HIGH);
     delayMicroseconds(2000);
-    digitalWrite(STEP, LOW);
+    digitalWrite(STEP_L, LOW);
+    delayMicroseconds(2000);
+
+    // rotate right stepping motor
+    digitalWrite(STEP_R, HIGH);
+    delayMicroseconds(2000);
+    digitalWrite(STEP_R, LOW);
     delayMicroseconds(2000);
   }
 
   delay(1000);
 
-  digitalWrite(DIR, LOW);
+  digitalWrite(DIR_L, LOW);
+  digitalWrite(DIR_R, LOW);
   
   for (int i=0; i<200; i++) {
-    digitalWrite(STEP, HIGH);
+    // rotate light stepping motor
+    digitalWrite(STEP_L, HIGH);
     delayMicroseconds(2000);
-    digitalWrite(STEP, LOW);
+    digitalWrite(STEP_L, LOW);
+    delayMicroseconds(2000);
+
+    // rotate right stepping motor
+    digitalWrite(STEP_R, HIGH);
+    delayMicroseconds(2000);
+    digitalWrite(STEP_R, LOW);
     delayMicroseconds(2000);
   }
 
