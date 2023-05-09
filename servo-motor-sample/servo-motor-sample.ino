ac5e20c379375ef2845b2607bfb3f829ddc89a91
keigo1216 <sagankeigo17@gmail.com>
2023-05-09 14:28:30 +0900

    add servo motor sample program

---
+++ servo-motor-sample/servo-motor-sample.ino
@@ -0,0 +1,22 @@
+#include <Servo.h> 
+Servo servo;
+ 
+const int IN = 2;
+
+void setup() {
+ 
+  servo.attach(IN);   // デジタルピンD3にサーボのSignal(オレンジ)を繋ぐ
+ 
+}
+ 
+void loop() {
+  
+  servo.write(0);    // サーボを0°の位置に動かす
+  delay(1000);
+ 
+  servo.write(90);   // サーボを90°の位置に動かす
+  delay(1000);
+ 
+  servo.write(180);  // サーボを180°の位置に動かす
+  delay(1000);
+ 
