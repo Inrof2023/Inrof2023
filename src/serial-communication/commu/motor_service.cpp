f4f934d224368828ff3563b354e2ef78cf3cf685
keigo1216 <sagankeigo17@gmail.com>
2023-05-31 09:52:48 +0900

    add interput function for line trace

--- src/serial-communication/commu/motor_service.cpp
+++ src/serial-communication/commu/motor_service.cpp
@@ -1,5 +1,65 @@
 #include "motor_service.hpp"
 
+// グローバル変数の宣言
+// 他の変数名と被らないように
+// ###################################################################################################
+// ライントレースの方向を格納
+enum class DirectionForLineTrace {
+  Forward,
+  Backward,
+};
+
+DirectionForLineTrace direction_for_line_trace = DirectionForLineTrace::Forward; // ライントレースの方向
+int SPEED_LEFT_FOR_LINE_TRACE = 100; // 左のステッピングモータの速度（1秒間の回転数）, 200が最大, 0が最小（停止）
+int SPEED_RIGHT_FOR_LINE_TRACE = 100; // 右のステッピングモータの速度（一秒間の回転数）, 200が最大, 0が最小（停止）
+int COUNT_STEPS_FOR_LINE_TRACE = 0; // ステッピングモータのステップ数をカウントする, 200が最大, 0が最小
+
+float PID_FOR_LINE_TRACE_PRESEN = 0; // 前回の偏差
+float PID_FOR_LINE_TRACE_INTEGRAL = 0; // 偏差の積分値
+
+// 割り込みで設定する関数
+// グローバル変数を使いまくる
+void interrupt(){
+  if (COUNT_STEPS_FOR_LINE_TRACE % 100 == 0) {
+    float sensor_val = (analogRead(LEFT) + analogRead(SENTER_L) - analogRead(SENTER_R) - analogRead(RIGHT)) / 2 / 1024.0;
+    float pid_val = PID_FOR_LINE_TRACE_P * sensor_val + PID_FOR_LINE_TRACE_I * PID_FOR_LINE_TRACE_INTEGRAL + PID_FOR_LINE_TRACE_D * (sensor_val - PID_FOR_LINE_TRACE_PRESEN);
+    if (pid_val > 100) pid_val = 100; // 最大値を超えないように
+    PID_FOR_LINE_TRACE_PRESEN = sensor_val;
+    PID_FOR_LINE_TRACE_INTEGRAL += sensor_val;
+    SPEED_LEFT_FOR_LINE_TRACE = 100 - pid_val;
+    SPEED_RIGHT_FOR_LINE_TRACE = 100 + pid_val;
+  }
+
+  // ライントレースする方向を指定
+  switch (direction_for_line_trace)
+  {
+  case DirectionForLineTrace::Forward:
+    digitalWrite(DIR_LEFT, LOW);
+    digitalWrite(DIR_RIGHT, HIGH);
+    break;
+  case DirectionForLineTrace::Backward:
+    digitalWrite(DIR_LEFT, HIGH);
+    digitalWrite(DIR_RIGHT, LOW);
+  }
+
+  // 左のモータを動かす
+  // 200 / SPEED_LEFT_FOR_LINE_TRACE がパルスを出す周期
+  // これがCOUNT_STEPS_FOR_LINE_TRACEの値を割り切れる時回転する
+  // ここは改善の余地がある（パルス幅をもう少し小さくするとか？）
+  if(SPEED_LEFT_FOR_LINE_TRACE > 0 && COUNT_STEPS_FOR_LINE_TRACE % (200 / SPEED_LEFT_FOR_LINE_TRACE) == 0){
+    digitalWrite(STEP_LEFT,HIGH);
+    digitalWrite(STEP_LEFT,LOW);
+  }
+  if(SPEED_LEFT_FOR_LINE_TRACE > 0 && COUNT_STEPS_FOR_LINE_TRACE % (200 / SPEED_RIGHT_FOR_LINE_TRACE) == 0){
+    digitalWrite(STEP_RIGHT,HIGH);
+    digitalWrite(STEP_RIGHT,LOW);
+  }
+
+  COUNT_STEPS_FOR_LINE_TRACE = (COUNT_STEPS_FOR_LINE_TRACE + 1) % 200;
+}
+
+// ###################################################################################################
+
 MotorService::MotorService() {
     SteppingMotor stepping_motor;
     ServoMotor servo_motor;
@@ -14,18 +74,18 @@ void MotorService::setup() {
     this->servo_motor.setup();
 }
 
-// 1byteのデータから必要なバイトを取り出す
-int MotorService::getMotorDataFromByte(Motor motor, char serial_data) {
-  switch (motor)
-  {
-  case Motor::STEPPING: // 後ろの3bitを取得する, ステッピングモータ
-    return serial_data & 0b00000111;
-  case Motor::SERVO: // 後ろから4bit目を取得する, サーボモータ
-    return serial_data & 0b00001000;
-  case Motor::DC: // 後ろから5bit目を取得する, DCモータ
-    return serial_data & 0b00010000;
-  }
-}
+// // 1byteのデータから必要なバイトを取り出す
+// int MotorService::getMotorDataFromByte(Motor motor, char serial_data) {
+//   switch (motor)
+//   {
+//   case Motor::STEPPING: // 後ろの3bitを取得する, ステッピングモータ
+//     return serial_data & 0b00000111;
+//   case Motor::SERVO: // 後ろから4bit目を取得する, サーボモータ
+//     return serial_data & 0b00001000;
+//   case Motor::DC: // 後ろから5bit目を取得する, DCモータ
+//     return serial_data & 0b00010000;
+//   }
+// }
 
 // 1byteのデータから必要なバイトを取り出す
 int MotorService::getDataFromByte(BitData bit_data, char serial_data) {
@@ -46,9 +106,32 @@ int MotorService::getDataFromByte(BitData bit_data, char serial_data) {
 
 void MotorService::driveMotor(char serial_data) {
     // ステッピングモータ
-    this->stepping_motor.moveSteppingMotor(MotorService::getMotorDataFromByte(Motor::STEPPING, serial_data), 1);
+    // ロボットの制御方法（ライントレースまたはカメラ）を決定する
+    if (MotorService::getDataFromByte(BitData::LINETRACE, serial_data)) {
+      // ライントレース
+
+      // ライントレースする時の方向（前進、後退）を決定する
+      // 割り込みする関数にも使うのでグローバル変数に代入する
+      
+
+      // StateがLINETRACEではない時（カメラからライントレースに切り替わる時）は割り込みを再開させる
+      // StateがLINETRACEの時は何もしない
+
+      // StateをLINETRACEに変更
+    }
+    else {
+      // カメラ
+
+      // StateがLINETRACEの時は割り込みを停止させる
+
+      // StateをCAMERAに変更
+
+      // ラズパイから送られてきたデータを元にロボットを動かす
+    }
+    // ステッピングモータ
+    this->stepping_motor.moveSteppingMotor(MotorService::getDataFromByte(BitData::STEPPING, serial_data), 1);
     // サーボモータ
-    this->servo_motor.moveServoMotor(MotorService::getMotorDataFromByte(Motor::SERVO, serial_data));
+    this->servo_motor.moveServoMotor(MotorService::getDataFromByte(BitData::SERVO, serial_data));
     // DCモータ
     // this->dc_motor.moveDCMotor(MotorService::getMotorDataFromByte(Motor::DC, serial_data));
 }
--- src/serial-communication/commu/motor_service.hpp
+++ src/serial-communication/commu/motor_service.hpp
@@ -7,13 +7,23 @@
 #include "servo_motor.hpp"
 // #include "dc_motor.hpp"
 
+// フォトリフレクタのピン番号
+#define LEFT A0
+#define SENTER_L A1
+#define SENTER_R A2
+#define RIGHT A3
+
+#define PID_FOR_LINE_TRACE_P 20
+#define PID_FOR_LINE_TRACE_I 0
+#define PID_FOR_LINE_TRACE_D 0
+
 class MotorService {
     private:
-        enum class Motor {
-            STEPPING,
-            SERVO,
-            DC
-        };
+        // enum class Motor {
+        //     STEPPING,
+        //     SERVO,
+        //     DC
+        // };
         enum class State {
             LINETRACE,
             CAMERA,
@@ -28,7 +38,7 @@ class MotorService {
         SteppingMotor stepping_motor;
         ServoMotor servo_motor;
         // DCMotor dc_motor;
-        int getMotorDataFromByte(Motor motor, char serial_data);
+        // int getMotorDataFromByte(Motor motor, char serial_data);
         int getDataFromByte(BitData bit_data, char serial_data);
 
     public:
