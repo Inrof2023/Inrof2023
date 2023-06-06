ba72a60ae4f07ef4009fe45b09abd7d5d9dcd040
keigo1216 <sagankeigo17@gmail.com>
2023-06-06 22:18:29 +0900

    update motor param

--- src/serial-communication/commu/constants.hpp
+++ src/serial-communication/commu/constants.hpp
@@ -12,8 +12,8 @@
 #define DC_PIN 3 // DCモーターのピン番号
 
 #define SERVO_PIN 2 // サーボモーターのピン番号
-#define UP_ANGLE -5 // サーボモーターの上向きの角度
-#define DOWN_ANGLE 90 // サーボモーターの下向きの角度
+#define UP_ANGLE 90 // サーボモーターの上向きの角度
+#define DOWN_ANGLE 180 // サーボモーターの下向きの角度
 
 //ステッピングモーター左
 #define DIR_LEFT 4
